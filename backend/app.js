9c6846e41aa39f67a36136218507ba8b16fa158e
Gustavo-Zamboni <gamegutaz@gmail.com>
2022-08-13 11:58:47 -0300

    Apostila 05 - fim da secao 2.2
    mapeamento de _id para id

--- backend/app.js
+++ backend/app.js
@@ -59,6 +59,7 @@ app.post('/api/clientes', (req, res, next) => {
 
 app.get('/api/clientes', (req, res, next) => {
   Cliente.find().then((documents) => {
+    console.log(documents)
     res.json({
       mensagem: "Tudo OK",
       clientes: documents
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Cliente } from "./cliente.model";
 import { Subject } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators"
 
 @Injectable({ providedIn: 'root' })
 export class ClienteService {
@@ -12,9 +13,15 @@ export class ClienteService {
 
   getClientes(): void {
     this.httpClient.get<{mensagem: string,
-      clientes: Cliente[]}>('http://localhost:3000/api/clientes').subscribe(
-        (dados) => {
-          this.clientes = dados.clientes;
+      clientes: any}>('http://localhost:3000/api/clientes')
+      .pipe(map((dados) => {
+        return dados.clientes.map(cliente => {
+          return {id: cliente._id, nome: cliente.nome, fone: cliente.fone, email: cliente.email}
+        })
+      }))
+      .subscribe(
+        (clientes) => {
+          this.clientes = clientes;
           this.listaClientesAtualizada.next([...this.clientes]);
         }
       )
@@ -24,7 +31,7 @@ export class ClienteService {
     const cliente: Cliente = {
       nome: nome,
       fone: fone,
-      email: email,
+      email: email
     };
     this.httpClient.post<{mensagem: string}>('http://localhost:3000/api/clientes',
     cliente).subscribe(
